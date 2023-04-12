f2a11d11aaccdcd74759b38553dcf01397d70e1b
Khash-Erdene ' B <Khash-Erdene2022Hop2c@NEST-96s-iMac.local>
2023-04-12 17:36:21 +0800

    login && register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import emailicon from "../assets/A.Tengis/emailicon.png";
-import lock from "../assets/A.Tengis/Lock.png";
-import googleicon from "../assets/A.Tengis/googleicon.png";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import emailicon from '../assets/A.Tengis/emailicon.png';
+import lock from '../assets/A.Tengis/Lock.png';
+import googleicon from '../assets/A.Tengis/googleicon.png';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+
+  const log = async (e) => {
+    e.preventDefault();
+    const res = await axios.post('http://localhost:8000/auth/login', {
+      email: email,
+      password: pw,
+    });
+    if (res?.status === 200) {
+      alert('Successfully logged in!');
+      navigate('/');
+    }
+  };
+
   return (
     <div className="relative flex flex-col justify-center overflow-hidden h-[87.9vh] bg-[#111]">
       <div className=" p-8 w-3/5 m-auto bg-white shadow-xl max-w-md shadow-orange-100">
@@ -20,6 +38,7 @@ const Login = () => {
                 type="email"
                 className="bg-white border border-gray-300 text-black text-sm focus:ring-orange-500 focus:border-orange-500 block w-full pl-10 p-2.5 "
                 placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
           </div>
@@ -33,28 +52,24 @@ const Login = () => {
                 type="password"
                 className="bg-white border border-gray-300 text-black text-sm focus:ring-orange-500 focus:border-orange-500 block w-full pl-10 p-2.5 "
                 placeholder="Password"
+                onChange={(e) => setPw(e.target.value)}
               />
             </div>
           </div>
           <div className="checkbox">
-            <input
-              type="checkbox"
-              id="scales"
-              name="scales"
-              className="accent-orange-500 mr-2"
-            />
+            <input type="checkbox" id="scales" name="scales" className="accent-orange-500 mr-2" />
             <label htmlFor="scales">Remember me?</label>
           </div>
           <div className="mt-6 flex justify-center pt-3">
-            <button className="w-screen px-5 py-3 tracking-wide text-white font-semibold transition-colors duration-200 transform bg-orange-400 rounded-md hover:bg-orange-600 focus:outline-none">
+            <button
+              onClick={log}
+              className="w-screen px-5 py-3 tracking-wide text-white font-semibold transition-colors duration-200 transform bg-orange-400 rounded-md hover:bg-orange-600 focus:outline-none"
+            >
               Log In
             </button>
           </div>
           <p className="mt-3 flex text-sm mb-8 font-light text-end justify-end text-gray-700">
-            <Link
-              to="/forgotpassword"
-              className=" text-gray-400 hover:underline"
-            >
+            <Link to="/forgotpassword" className=" text-gray-400 hover:underline">
               Forgot password?
             </Link>
           </p>
--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const PORT = 8000;
 const MONGO_URI =
-  "mongodb+srv://Khash-Erdene:Itachi07@cluster0.79uixyi.mongodb.net/bogino";
+  "mongodb+srv://Khash-Erdene:Itachi07@cluster0.79uixyi.mongodb.net/hoolly";
 const app = express();
 const auth = require("./routers/auth");
+const dotenv = require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
