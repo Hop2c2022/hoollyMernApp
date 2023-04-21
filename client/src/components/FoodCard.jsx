24249a2bb945f92f273d3e06e383ce99954b9660
2022Hop2c-A.Tengis <2022Hop2c-A.Tengis@Mandakh.local>
2023-04-21 19:38:44 +0800

    asdfg

--- client/src/components/FoodCard.jsx
+++ client/src/components/FoodCard.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import axios from 'axios';
+import Loader from './reloadcard';
 
 const FoodCard = () => {
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const [foods, setFoods] = useState([]);
   let val = window.location.search.split('?')[1];
@@ -10,43 +12,54 @@ const FoodCard = () => {
     if (val == 'breakfast') {
       const res = await axios.get('http://localhost:8000/orders/breakfast');
       setFoods(res?.data?.result);
+      setLoading(false);
     } else if (val == 'dinner') {
       const res = await axios.get('http://localhost:8000/orders/dinner');
       setFoods(res?.data?.result);
+      setLoading(false);
     } else {
       const res = await axios.get('http://localhost:8000/orders/lunch');
       setFoods(res?.data?.result);
+      setLoading(false);
     }
   };
+
+  const urt = foods;
 
   useState(() => {
     orders();
   }, [orders]);
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-12 mt-12 mb-12 xl:grid-cols-3 ">
-      {foods?.map((el) => {
-        return (
-          <div className="bg-slay-300 hover:bg-white hover:text-black  text-[#fff] border border-black-100 transition transform duration-700 hover:scale-105 p-4 relative w-96 hover:shadow-gray-400 hover:shadow-xl rounded-lg">
-            <p className="bg-blue-500 border border-blue-500 rounded-full text-primary text-sm poppins px-4 py-1 inline-block mb-4">
-              {el?.type}
-            </p>
-            <img className="w-64 h-64 object-contain mx-auto " src={el?.image} />
-            <div className="flex flex-col items-center my-3 space-y-2 gap-3">
-              <h1 className=" poppins text-lg">{el?.title}</h1>
-              <p className=" poppins text-sm text-center">From: {el?.brand}</p>
-              <h2 className=" poppins text-2xl font-bold">{el?.price}</h2>
-              <button
-                onClick={() => navigate(`/shoplist?${el?._id}`)}
-                className="bg-blue-800 text-[#fff] px-8 py-2 focus:outline-none poppins rounded-full transform transition duration-300 hover:scale-105"
-              >
-                Order Now
-              </button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-12 mt-12 mb-12 xl:grid-cols-3 ">
+          {foods?.map((el) => {
+            return (
+              <div className="bg-slay-300 hover:bg-white hover:text-black  text-[#fff] border border-black-100 transition transform duration-700 hover:scale-105 p-4 relative w-96 hover:shadow-gray-400 hover:shadow-xl rounded-lg">
+                <p className="bg-blue-500 border border-blue-500 rounded-full text-primary text-sm poppins px-4 py-1 inline-block mb-4">
+                  {el?.type}
+                </p>
+                <img className="w-64 h-64 object-contain mx-auto " src={el?.image} />
+                <div className="flex flex-col items-center my-3 space-y-2 gap-3">
+                  <h1 className=" poppins text-lg">{el?.title}</h1>
+                  <p className=" poppins text-sm text-center">From: {el?.brand}</p>
+                  <h2 className=" poppins text-2xl font-bold">{el?.price}</h2>
+                  <button
+                    onClick={() => navigate(`/shoplist?${el?._id}`)}
+                    className="bg-blue-800 text-[#fff] px-8 py-2 focus:outline-none poppins rounded-full transform transition duration-300 hover:scale-105"
+                  >
+                    Order Now
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
