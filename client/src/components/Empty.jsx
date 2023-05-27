884de0fca714bab81fa51a1acacd1e78307515b1
Tengis's Macbook <tengis2007ariunbold@gmail.com>
2023-05-27 15:36:55 +0800

    asd

--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 const Empty = () => {
   return (
     <div className=" flex flex-col items-center justify-center overflow-hidden h-[83vh] mt-[0.2vh] text-center ">
-      <h1 className="text-orange-400 font-semibold md:text-8xl text-6xl">😔</h1>
-      <h1 className="md:text-3xl text-2xl pt-[1.5%] text-white">Oops! This Restaurant don't have items</h1>
-      <p className="text-center md:text-xl text-md pt-[1%] pb-[1%] text-gray-500">
+      <h1 className="text-orange-400 font-semibold md:text-8xl text-4xl">😔</h1>
+      <h1 className="md:text-3xl text-1xl pt-[1.5%] text-white">Oops! This Restaurant don't have items</h1>
+      <p className="text-center md:text-xl text-sm pt-[1%] pb-[1%] text-gray-500">
         Page Cannot be found! we’ll have it figured out in no time.
         <br />
         Menwhile, cheek out these fresh ideas:
       </p>
       <Link to="/">
-        <button className=" h-12 md:px-6 px-4 tracking-wide text-white font-semibold transition-colors duration-200 transform bg-orange-400 rounded-md hover:bg-ornage-800 focus:outline-none">
+        <button className=" h-12 md:px-6 px-3 tracking-wide text-white font-semibold transition-colors duration-200 transform bg-orange-400 rounded-md hover:bg-ornage-800 focus:outline-none">
           Go to Home
         </button>
       </Link>
